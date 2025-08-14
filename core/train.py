4c46cc7f66b42323ae84d509eba771cfd97ecea4
chenyinbo <chenyinbo@sics.ac.cn>
2025-08-14 14:33:55 +0800

    fix(*): 修正 info_nce 的计算方式避免数值溢出

--- core/train.py
+++ core/train.py
@@ -1,10 +1,9 @@
 import torch
+import torch.nn.functional as functional
 
 
 def info_nce_loss(questions_embedding: torch.Tensor, images_embedding: torch.Tensor, temperature: float = 0.07):
     similarity_matrix = torch.matmul(questions_embedding, images_embedding.T) / temperature
-    logit_positive_pairs = torch.diag(similarity_matrix)
-    exp_logits = torch.exp(similarity_matrix)
-    sum_exp_logits = exp_logits.sum(dim=1)
-    loss = - (logit_positive_pairs - torch.log(sum_exp_logits)).mean()
+    labels = torch.arange(len(questions_embedding)).to(questions_embedding.device)
+    loss = functional.cross_entropy(similarity_matrix, labels)
     return loss
